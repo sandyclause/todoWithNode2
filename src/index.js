0b4b6a0a7852111ad2851691827dfa3df05e9db4
sandy <sandytian2000@hotmail.com>
2019-09-21 15:07:38 -0400

    POST & GET for task

--- src/index.js
+++ src/index.js
@@ -30,7 +30,6 @@ app.get('/users/:id', (req, res) => {
   const _id = req.params.id
 
   User.findById(_id).then((user) => {
-    console.log(user)
     if (!user) {
       return res.status(404).send()
     }
@@ -48,6 +47,28 @@ app.post('/tasks', (req, res) => {
     res.status(201).send(task)
   }).catch((e) => {
     res.status(400).send(e)
+  })
+})
+
+app.get('/tasks', (req, res) => {
+  Task.find({}).then((task) => {
+    res.send(task)
+  }).catch((e) => {
+    res.status(500).send()
+  })
+})
+
+app.get('/tasks/:id', (req, res) => {
+  const _id = req.params.id
+
+  Task.findById(_id).then((task) => {
+    if (!task) {
+      return res.status(404).send()
+    }
+
+    res.send(task)
+  }).catch((e) => {
+    res.status(500).send()
   })
 })
 
