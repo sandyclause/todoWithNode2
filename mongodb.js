0c40f6c5ef15b341fc8e097c85452c7afab35d1c
sandy <sandytian2000@hotmail.com>
2019-09-20 21:15:05 -0400

    inital commit

---
+++ mongodb.js
@@ -0,0 +1,17 @@
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager2'
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+  if (error) {
+    return console.log('unable to connect to database')
+  }
+
+  const db = client.db(databaseName)
+  db.collection('users').insertOne({
+    name: 'sandy',
+    age: 80
+  })
+})
