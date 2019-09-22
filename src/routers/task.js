1abe30a19bdd34c05821998ab5f39dd409168227
sandy <sandytian2000@hotmail.com>
2019-09-22 12:03:25 -0400

    task middleware setup

--- src/routers/task.js
+++ src/routers/task.js
@@ -45,7 +45,14 @@ router.patch('/tasks/:id', async (req, res) => {
     return res.status(400).send({error: 'invalid updates'})
   }
   try {
-    const task = await Task.findByIdAndUpdate(_id, req.body, { new: true, runValidators: true})
+    const task = await Task.findById(_id)
+
+    updates.forEach((update) => {
+      return task[update] = req.body[update]
+    })
+
+    await task.save()
+
     if (!task) {
       return res.status(404).send()
     }
