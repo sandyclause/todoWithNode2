762d4b11f8f17855ba55de1d18537efa3370aea6
sandy <sandytian2000@hotmail.com>
2019-09-23 20:26:32 -0400

    log in post endpoint

--- src/models/user.js
+++ src/models/user.js
@@ -13,6 +13,7 @@ const userSchema = new mongoose.Schema({
     required: true,
     trim: true,
     lowercase: true,
+    unique: true,
     validate(value) {
       if (!validator.isEmail(value)) {
         throw new Error('email is invalid')
@@ -40,6 +41,21 @@ const userSchema = new mongoose.Schema({
     required: true,
   }
 })
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({email})
+  if (!user) {
+    throw new Error('unable to login')
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password)
+
+  if (!isMatch) {
+    throw new Error('unable to login')
+  }
+
+  return user
+}
 
 userSchema.pre('save', async function(next) {
   const user = this
